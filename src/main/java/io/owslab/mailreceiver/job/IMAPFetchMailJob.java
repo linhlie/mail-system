75294575834ca32469d00a10176d8c2d7aac907d
LVBK <levanbaokhanh.bkhn@gmail.com>
2018-01-22 04:54:57 +0700

    storage mail content

--- src/main/java/io/owslab/mailreceiver/job/IMAPFetchMailJob.java
+++ src/main/java/io/owslab/mailreceiver/job/IMAPFetchMailJob.java
@@ -12,6 +12,7 @@ import javax.mail.*;
 import javax.mail.internet.InternetAddress;
 import javax.mail.internet.MimeMessage;
 import javax.mail.search.*;
+import java.io.IOException;
 import java.text.SimpleDateFormat;
 import java.util.*;
 
@@ -142,34 +143,42 @@ public class IMAPFetchMailJob implements Runnable {
         }
     }
 
-    private String getContentText(Part p) throws Exception {
+    private String getContentText(Part p) throws MessagingException, IOException {
 
-        if (p.isMimeType("text/plain")) {
-            return (String) p.getContent();
+        if (p.isMimeType("text/*")) {
+            String s = (String)p.getContent();
+            return s;
         }
-        //check if the content has attachment
-        else if (p.isMimeType("multipart/*")) {
-            Multipart mp = (Multipart) p.getContent();
-            int count = mp.getCount();
-            String result = "";
-            for (int i = 0; i < count; i++) {
-                result = result + "\n"  + getContentText(mp.getBodyPart(i));
+
+        if (p.isMimeType("multipart/alternative")) {
+            // prefer html text over plain text
+            Multipart mp = (Multipart)p.getContent();
+            String text = null;
+            for (int i = 0; i < mp.getCount(); i++) {
+                Part bp = mp.getBodyPart(i);
+                if (bp.isMimeType("text/plain")) {
+                    if (text == null)
+                        text = getContentText(bp);
+                    continue;
+                } else if (bp.isMimeType("text/html")) {
+                    String s = getContentText(bp);
+                    if (s != null)
+                        return s;
+                } else {
+                    return getContentText(bp);
+                }
             }
-            return result;
-        }
-        //check if the content is a nested message
-        else if (p.isMimeType("message/rfc822")) {
-            return getContentText((Part) p.getContent());
-        }
-        else {
-            Object o = p.getContent();
-            if (o instanceof String) {
-                return (String) o;
-            }
-            else {
-                return o.toString();
+            return text;
+        } else if (p.isMimeType("multipart/*")) {
+            Multipart mp = (Multipart)p.getContent();
+            for (int i = 0; i < mp.getCount(); i++) {
+                String s = getContentText(mp.getBodyPart(i));
+                if (s != null)
+                    return s;
             }
         }
+
+        return null;
     }
 
     private String buildMessageId(MimeMessage message, ReceiveEmailAccountSetting account) throws MessagingException {
