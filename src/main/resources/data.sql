b10b5f6de6d219bc92eb0fd0c3094f16fb62ff1f
LVBK <levanbaokhanh.bkhn@gmail.com>
2018-01-31 16:42:56 +0700

    init and migration sql

---
+++ src/main/resources/data.sql
@@ -0,0 +1,6 @@
+DROP DATABASE IF EXISTS `mailsys`;
+CREATE DATABASE `mailsys` CHARACTER SET utf8mb4 COLLATE utf8mb4_unicode_ci;
+USE `mailsys`;
+
+SET NAMES utf8mb4 COLLATE utf8mb4_unicode_ci;
+SET FOREIGN_KEY_CHECKS=0;
---
+++ src/main/resources/db/migration/V1__init.sql
@@ -0,0 +1,74 @@
+CREATE TABLE `Key_Values` (
+  `key` VARCHAR(200) PRIMARY KEY,
+  `value` TEXT DEFAULT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_unicode_ci;
+
+CREATE TABLE `Users` (
+  username VARCHAR(50)  NOT NULL PRIMARY KEY,
+  password VARCHAR(255) NOT NULL,
+  enabled  BOOLEAN      NOT NULL
+) ENGINE = InnoDB;
+
+CREATE TABLE `Authorities` (
+  username  VARCHAR(50) NOT NULL,
+  authority VARCHAR(50) NOT NULL,
+  FOREIGN KEY (username) REFERENCES users (username),
+  UNIQUE INDEX authorities_idx_1 (username, authority)
+) ENGINE = InnoDB;
+
+CREATE TABLE `Receive_Email_Account_Settings` (
+  `id` INT AUTO_INCREMENT PRIMARY KEY,
+  `account` VARCHAR(60) NOT NULL,
+  `password` VARCHAR(32) NOT NULL,
+  `mail_server_address` VARCHAR(200) NOT NULL,
+  `mail_server_port` INT NOT NULL,
+  `receive_mail_protocol` SMALLINT(6) NOT NULL DEFAULT '0' COMMENT '0、IMAP 1. POP3',
+  `encryption_protocol`SMALLINT(6) NOT NULL DEFAULT '0'COMMENT '0、なし 1. SSL/TLS, 2. STARTTLS',
+  `authentication_protocol` SMALLINT(6) NOT NULL DEFAULT '0' COMMENT '0. 通常のパスワード認証 1. 暗号化されたパスワード認証 2. Kerberos/GSSAPI 3. NTLM 4. TLS証明書 5. OAuth2',
+  `proxy_server` VARCHAR(200) DEFAULT NULL,
+  `disabled` BOOLEAN DEFAULT FALSE,
+  `created_at` DATETIME DEFAULT NULL,
+  `updated_at` DATETIME DEFAULT NULL,
+  UNIQUE KEY unique_account (account)
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_unicode_ci;
+
+CREATE TABLE `Emails` (
+  `message_id` VARCHAR(200) PRIMARY KEY,
+  `account_id` INT NOT NULL,
+  `from` VARCHAR(60) NOT NULL,
+  `subject` TEXT COLLATE utf8mb4_unicode_ci NOT NULL,
+  `to` TEXT NOT NULL,
+  `cc` TEXT DEFAULT NULL,
+  `bcc` TEXT DEFAULT NULL,
+  `reply_to` VARCHAR(60) DEFAULT NULL,
+  `sent_at` DATETIME NOT NULL,
+  `received_at` DATETIME DEFAULT NULL,
+  `has_attachment` BOOLEAN DEFAULT FALSE,
+  `content_type` SMALLINT(6) NOT NULL DEFAULT '0' COMMENT '0、TEXT 1. HTML',
+  `original_body`TEXT COLLATE utf8mb4_unicode_ci DEFAULT NULL,
+  `optimized_body`TEXT COLLATE utf8mb4_unicode_ci DEFAULT NULL,
+  `header` TEXT DEFAULT NULL,
+  `created_at` DATETIME DEFAULT NULL,
+  `meta_data` TEXT COLLATE utf8mb4_unicode_ci DEFAULT NULL,
+  `deleted` BOOLEAN DEFAULT FALSE,
+  `deleted_at` DATETIME DEFAULT NULL,
+  FOREIGN KEY fk_receive_email_account(account_id)
+  REFERENCES Receive_Email_Account_Settings(id)
+    ON UPDATE CASCADE
+    ON DELETE CASCADE
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_unicode_ci;
+
+CREATE TABLE `Files` (
+  `id` INT AUTO_INCREMENT PRIMARY KEY,
+  `message_id` VARCHAR(200) NOT NULL,
+  `file_name` VARCHAR(60) NOT NULL,
+  `storage_path` VARCHAR(200) NOT NULL,
+  `created_at` DATETIME DEFAULT NULL,
+  `meta_data` TEXT DEFAULT NULL,
+  `deleted` BOOLEAN DEFAULT FALSE,
+  `deleted_at` DATETIME DEFAULT NULL,
+  FOREIGN KEY fk_receive_email(message_id)
+  REFERENCES Emails(message_id)
+    ON UPDATE CASCADE
+    ON DELETE CASCADE
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_unicode_ci;
