3c9504eca34c70ce7a40418370dddee52cc5cc3d
LVBK <levanbaokhanh.bkhn@gmail.com>
2018-02-01 17:46:45 +0700

    add 404 and 500 error page

--- src/main/java/io/owslab/mailreceiver/config/WebSecurityConfig.java
+++ src/main/java/io/owslab/mailreceiver/config/WebSecurityConfig.java
@@ -30,7 +30,8 @@ public class WebSecurityConfig extends WebSecurityConfigurerAdapter {
 
     @Override
     protected void configure(HttpSecurity http) throws Exception {
-        http.authorizeRequests()
+        http
+                .authorizeRequests()
                 .antMatchers("/bootstrap/**", "/dist/**", "/plugins/**").permitAll()
                 .anyRequest().authenticated()
                 .and()
