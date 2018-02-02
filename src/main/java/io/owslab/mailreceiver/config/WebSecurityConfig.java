a0d22299ed4ed736d80f3e3ac9e4a6d0e8fd0712
LVBK <levanbaokhanh.bkhn@gmail.com>
2018-02-02 10:55:39 +0700

    add custom css

--- src/main/java/io/owslab/mailreceiver/config/WebSecurityConfig.java
+++ src/main/java/io/owslab/mailreceiver/config/WebSecurityConfig.java
@@ -32,7 +32,7 @@ public class WebSecurityConfig extends WebSecurityConfigurerAdapter {
     protected void configure(HttpSecurity http) throws Exception {
         http
                 .authorizeRequests()
-                .antMatchers("/bootstrap/**", "/dist/**", "/plugins/**").permitAll()
+                .antMatchers("/bootstrap/**", "/dist/**", "/plugins/**", "/custom/**").permitAll()
                 .anyRequest().authenticated()
                 .and()
                 .formLogin()
