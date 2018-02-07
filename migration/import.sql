30c4b4d7e903edd7d6673bcd311e4213dd81ff78
LVBK <levanbaokhanh.bkhn@gmail.com>
2018-02-07 11:59:14 +0700

    update sql migration

--- migration/import.sql
+++ migration/import.sql
@@ -99,4 +99,34 @@ CREATE TABLE `Files` (
     ON DELETE CASCADE
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_unicode_ci;
 
+DROP TABLE IF EXISTS `Words`;
+CREATE TABLE `Words` (
+  `id` INT AUTO_INCREMENT PRIMARY KEY,
+  `word` VARCHAR(191) NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_unicode_ci;
+
+INSERT INTO Words(id, word)
+VALUES (1, 'java');
+INSERT INTO Words(id, word)
+VALUES (2, 'JAVA');
+
+DROP TABLE IF EXISTS `Fuzzy_Words`;
+CREATE TABLE `Fuzzy_Words` (
+  `id` INT AUTO_INCREMENT PRIMARY KEY,
+  `word_id` INT NOT NULL,
+  `with_word_id` INT NOT NULL,
+  `fuzzy_type` SMALLINT(6) NOT NULL DEFAULT '1' COMMENT '0、除外 1. 同一',
+  FOREIGN KEY fk_word(word_id)
+  REFERENCES Words(id)
+    ON UPDATE CASCADE
+    ON DELETE CASCADE,
+  FOREIGN KEY fk_with_word(with_word_id)
+  REFERENCES Words(id)
+    ON UPDATE CASCADE
+    ON DELETE CASCADE
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_unicode_ci;
+
+INSERT INTO Fuzzy_Words(word_id, with_word_id, fuzzy_type)
+VALUES (1, 2, 1);
+
 COMMIT;
