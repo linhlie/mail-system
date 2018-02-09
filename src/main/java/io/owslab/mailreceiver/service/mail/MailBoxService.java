e4411921c4f9f20697a809cf2f8e53d07650c175
LVBK <levanbaokhanh.bkhn@gmail.com>
2018-02-09 12:13:07 +0700

    prepare optimize content for build match word data

--- src/main/java/io/owslab/mailreceiver/service/mail/MailBoxService.java
+++ src/main/java/io/owslab/mailreceiver/service/mail/MailBoxService.java
@@ -3,6 +3,7 @@ package io.owslab.mailreceiver.service.mail;
 import com.mariten.kanatools.KanaConverter;
 import io.owslab.mailreceiver.dao.EmailDAO;
 import io.owslab.mailreceiver.model.Email;
+import org.jsoup.Jsoup;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.data.domain.Page;
 import org.springframework.data.domain.PageRequest;
@@ -38,10 +39,12 @@ public class MailBoxService {
     }
 
     public static String optimizeText(String original){
-        int conv_op_flags = 0;
-        conv_op_flags |= KanaConverter.OP_HAN_KATA_TO_ZEN_KATA;
-        conv_op_flags |= KanaConverter.OP_ZEN_ASCII_TO_HAN_ASCII;
-        String optimizedText = KanaConverter.convertKana(original, conv_op_flags);
-        return  optimizedText.toLowerCase();
+//        int conv_op_flags = 0;
+//        conv_op_flags |= KanaConverter.OP_HAN_KATA_TO_ZEN_KATA;
+//        conv_op_flags |= KanaConverter.OP_ZEN_ASCII_TO_HAN_ASCII;
+//        String optimizedText = KanaConverter.convertKana(original, conv_op_flags);
+//        return  optimizedText.toLowerCase();
+        String optimizedText = Jsoup.parse(original).text();
+        return optimizedText.toLowerCase();
     }
 }
