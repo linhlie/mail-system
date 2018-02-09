5bcd4e246b672275ef1c28874d5367fbf8919aac
LVBK <levanbaokhanh.bkhn@gmail.com>
2018-02-09 16:02:09 +0700

    triiger email words

--- migration/import.sql
+++ migration/import.sql
@@ -146,6 +146,47 @@ CREATE TABLE `Emails_Words` (
     ON DELETE CASCADE
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_unicode_ci;
 
+INSERT INTO Emails_Words(message_id, word_id, appear_indexs)
+VALUES ('khanhlvb@ows.vn+<001a114425824f4509056431a819@google.com>', 2, '');
 
+DELIMITER #
+CREATE TRIGGER ins_email AFTER INSERT ON Emails
+FOR EACH ROW
+BEGIN
+  DECLARE done INT DEFAULT FALSE;
+  DECLARE c1 INT;
+  DECLARE cur CURSOR FOR SELECT id FROM Words;
+  DECLARE CONTINUE HANDLER FOR NOT FOUND SET done = TRUE;
+  OPEN cur;
+    ins_loop: LOOP
+      FETCH cur INTO c1;
+      IF done THEN
+        LEAVE ins_loop;
+      END IF;
+      INSERT INTO Emails_Words (message_id, word_id, appear_indexs) values (new.message_id, c1 , '');
+    END LOOP;
+  CLOSE cur;
+END#
+DELIMITER ;
+
+DELIMITER //
+CREATE TRIGGER ins_word AFTER INSERT ON Words
+FOR EACH ROW
+BEGIN
+  DECLARE done INT DEFAULT FALSE;
+  DECLARE c1 VARCHAR(200);
+  DECLARE cur CURSOR FOR SELECT message_id FROM Emails WHERE Emails.deleted = FALSE;
+  DECLARE CONTINUE HANDLER FOR NOT FOUND SET done = TRUE;
+  OPEN cur;
+    ins_loop: LOOP
+      FETCH cur INTO c1;
+      IF done THEN
+        LEAVE ins_loop;
+      END IF;
+      INSERT INTO Emails_Words (message_id, word_id, appear_indexs) values (c1, new.id , '');
+    END LOOP;
+  CLOSE cur;
+END//
+DELIMITER ;
 
 COMMIT;
