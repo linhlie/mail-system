dffafc4f4f7b290da29a78c61456be3760451475
LVBK <levanbaokhanh.bkhn@gmail.com>
2018-02-12 15:38:59 +0700

    basic number treatment form

--- src/main/resources/static/custom/js/pages/numberTreatment/form.js
+++ src/main/resources/static/custom/js/pages/numberTreatment/form.js
@@ -9,6 +9,8 @@
         setAddRowListener('addReplaceUnit', 'replaceUnit');
         setRemoveRowListener('removeReplaceNumber', 'replaceNumber');
         setRemoveRowListener('removeReplaceUnit', 'replaceUnit');
+        setAddReplaceLetterRowListener('addReplaceLetter', 'replaceLetter');
+        setRemoveRowListener('removeReplaceLetter', 'replaceLetter');
         setGoBackListener('backBtn');
     });
 
@@ -29,6 +31,32 @@
             var table = document.getElementById(tableId);
             var index = $(this)[0].parentNode.parentNode.rowIndex;
             table.deleteRow(index);
+        })
+    }
+
+    function setAddReplaceLetterRowListener(name, tableId) {
+        $("span[name='"+name+"']").click(function () {
+            var table = document.getElementById(tableId);
+            var row = table.insertRow(-1);
+            var cell1 = row.insertCell(0);
+            var cell2 = row.insertCell(1);
+            var cell3 = row.insertCell(2);
+            var cell4 = row.insertCell(3);
+            cell1.className = 'select-container';
+            cell3.className = 'select-container';
+            cell1.innerHTML = '<select class="form-control select2 select2-hidden-accessible" aria-hidden="true">' +
+                '<option value="" selected="selected" disabled="disabled">選択してください</option>' +
+                '<option value="1" >数値の前の</option>' +
+                '<option value="0" >数値の後の</option>' +
+                '</select>'
+            cell2.innerHTML = '<input class="text-center" type="text" placeholder="文字"/>';
+            cell3.innerHTML = '<select class="form-control select2 select2-hidden-accessible" aria-hidden="true">' +
+                '<option value="" selected="selected" disabled="disabled">選択してください</option>'
+                '<option value="1" >以上として認識する</option>' +
+                '<option value="2" >以下として認識する</option>' +
+                '<option value="3" >未満として認識する</option>' +
+                '<option value="4" >超として認識する</option>' +
+                '</select>'
         })
     }
 
