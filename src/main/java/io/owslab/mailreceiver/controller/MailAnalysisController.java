20caeed7dc824052af1a22c9fca400ae80c1844e
LVBK <levanbaokhanh.bkhn@gmail.com>
2018-02-28 10:59:40 +0700

    mail analysis empty page

---
+++ src/main/java/io/owslab/mailreceiver/controller/MailAnalysisController.java
@@ -0,0 +1,17 @@
+package io.owslab.mailreceiver.controller;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
+
+/**
+ * Created by khanhlvb on 2/28/18.
+ */
+@Controller
+public class MailAnalysisController {
+    @RequestMapping(value = "/mailAnalysis", method = RequestMethod.GET)
+    public String getMailAnalysisPage(Model model) {
+        return "mailAnalysis/form";
+    }
+}
