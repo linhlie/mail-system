01a9775b93b5151f3ddbce7af537cdf1848874d8
LVBK <levanbaokhanh.bkhn@gmail.com>
2018-03-02 17:22:04 +0700

    add AdministratorSetting empty page

--- src/main/java/io/owslab/mailreceiver/controller/AdministratorSettingController.java
+++ src/main/java/io/owslab/mailreceiver/controller/AdministratorSettingController.java
@@ -5,6 +5,9 @@ import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
 import org.springframework.security.crypto.password.PasswordEncoder;
 import org.springframework.security.provisioning.JdbcUserDetailsManager;
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
 
 import javax.sql.DataSource;
 
@@ -12,11 +15,13 @@ import javax.sql.DataSource;
  * Created by khanhlvb on 2/28/18.
  */
 @Controller
+@RequestMapping("/admin/")
 public class AdministratorSettingController {
-
-    @Autowired
-    private DataSource datasource;
 
     //TODO: change admin login id and password, validate with current password
     //TODO: only one id can login to admin system
+    @RequestMapping(value = { "/administratorSetting" }, method = RequestMethod.GET)
+    public String index(Model model) {
+        return "admin/setting";
+    }
 }
