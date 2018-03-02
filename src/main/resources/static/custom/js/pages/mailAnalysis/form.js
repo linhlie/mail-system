3e0d5d368b644c290e95c596d936d1b8c82d00a3
LVBK <levanbaokhanh.bkhn@gmail.com>
2018-03-02 16:40:39 +0700

    completed static matching condition group page

--- src/main/resources/static/custom/js/pages/mailAnalysis/form.js
+++ src/main/resources/static/custom/js/pages/mailAnalysis/form.js
@@ -1,6 +1,31 @@
 
 (function () {
-
     "use strict";
+
+    $(function () {
+        setAddReplaceLetterRowListener('addMotoRow', 'motoMail', ["group", "combine", "item", "condition", "value"]);
+        setAddReplaceLetterRowListener('addSakiRow', 'sakiMail', ["group", "combine", "item", "condition", "value"]);
+        setAddReplaceLetterRowListener('addMatchRow', 'matching', ["group", "combine", "item", "condition", "value"]);
+        setRemoveRowListener("removeConditionRow");
+    });
+
+    function setRemoveRowListener(name) {
+        $("span[name='"+name+"']").off('click');
+        $("span[name='"+name+"']").click(function () {
+            $(this)[0].parentNode.parentNode.className+=' hidden';
+        })
+    }
+
+    function setAddReplaceLetterRowListener(name, tableId, data) {
+        $("span[name='"+name+"']").click(function () {
+            var table = document.getElementById(tableId);
+            var body = table.tBodies[0];
+            var rowToClone = table.rows[1];
+            var clone = rowToClone.cloneNode(true);
+            clone.className = undefined;
+            body.appendChild(clone);
+            setRemoveRowListener("removeConditionRow");
+        })
+    }
 
 })(jQuery);
