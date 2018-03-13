977df172b1a56e36a7425924057e32aad7eb9e17
LVBK <levanbaokhanh.bkhn@gmail.com>
2018-03-13 14:54:01 +0700

    show matching result

--- src/main/resources/static/custom/js/pages/matching/result.js
+++ src/main/resources/static/custom/js/pages/matching/result.js
@@ -8,6 +8,7 @@
     matchingResultStr = sessionStorage.getItem("matchingResultData");
     matchingResultStr = matchingResultStr || "null";
     var matchingResult = null;
+    var currentDestinationResult = [];
     try {
         matchingResult  = JSON.parse(matchingResultStr);
     } catch (error) {
@@ -16,24 +17,42 @@
     console.log("window.matchingResultData: ", matchingResult);
 
     $(function () {
-        setRowClickListener("showMail", showMail);
-        setRowClickListener("sourceRow", selectedRow);
         showSourceData(sourceTableId, matchingResult);
     });
 
     function showSourceData(tableId, data) {
+        removeAllRow(tableId);
         if(data.length > 0){
             for(var i = 0; i < data.length; i ++){
-                addRowWithData(tableId, data[i]);
+                addRowWithData(tableId, data[i], i, function () {
+                    setRowClickListener("showSourceMail", showSourceMail);
+                    setRowClickListener("sourceRow", selectedRow);
+                });
+            }
+        }
+    }
+    
+    function showDestinationData(tableId, data) {
+        var word = data.word;
+        var source = data.source;
+        currentDestinationResult = data.destinationList;
+        removeAllRow(tableId);
+        if(currentDestinationResult.length > 0){
+            for(var i = 0; i < currentDestinationResult.length; i ++){
+                currentDestinationResult[i].word = word;
+                addRowWithData(tableId, currentDestinationResult[i], i, function () {
+                    setRowClickListener("showDestinationMail", showDestinationMail);
+                });
             }
         }
     }
 
-    function addRowWithData(tableId, data) {
+    function addRowWithData(tableId, data, index, callback) {
         var table = document.getElementById(tableId);
         var body = table.tBodies[0];
         var rowToClone = table.rows[1];
         var row = rowToClone.cloneNode(true);
+        row.setAttribute("data", index);
         row.className = undefined;
         var cells = row.cells;
         for(var i = 0; i < cells.length; i++){
@@ -45,7 +64,6 @@
             if(cellNode){
                 if(cellNode.nodeName == "SPAN") {
                     var cellData = cellKeys.length == 2 ? (data[cellKeys[0]] ? data[cellKeys[0]][cellKeys[1]] : undefined) : data[cellKeys[0]];
-                    console.log("cellData: ", typeof cellData, Array.isArray(cellData));
                     if(Array.isArray(cellData)){
                         cellNode.textContent = cellData.length;
                     } else {
@@ -55,25 +73,57 @@
             }
         }
         body.appendChild(row);
-        setRowClickListener("showMail", showMail);
-        setRowClickListener("sourceRow", selectedRow);
+        if(typeof callback === "function"){
+            callback(data);
+        }
     }
     
     function setRowClickListener(name, callback) {
         $("td[name='"+name+"']").off('click');
         $("td[name='"+name+"']").click(function () {
             if(typeof callback == "function"){
-                callback();
+                callback.apply(this);
             }
         })
     }
     
-    function showMail() {
-        console.log("showMail")
+    function showSourceMail() {
+        var row = $(this)[0].parentNode;
+        var index = row.getAttribute("data");
+        var rowData = matchingResult[index];
+        if(rowData && rowData.source && rowData.source.messageId){
+            showMail(rowData.source.messageId);
+        }
+        //TODO: show mail
+    }
+
+    function showDestinationMail() {
+        var row = $(this)[0].parentNode;
+        var index = row.getAttribute("data");
+        var rowData = currentDestinationResult[index];
+        if(rowData && rowData.messageId){
+            showMail(rowData.messageId);
+        }
     }
     
     function selectedRow() {
-        console.log("selectedRow");
+        var row = $(this)[0].parentNode;
+        var index = row.getAttribute("data");
+        var rowData = matchingResult[index];
+        showDestinationData(destinationTableId, rowData);
+    }
+
+    function removeAllRow(tableId) { //Except header row
+        var table = document.getElementById(tableId);
+        while(table.rows.length > 2){
+            var row = table.rows[2];
+            row.parentNode.removeChild(row);
+        }
+    }
+    
+    function showMail(messageId) {
+        console.log("showMail: ", messageId);
+        //TODO: show mail with message id
     }
 
 })(jQuery);
