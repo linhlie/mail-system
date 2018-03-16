91a1338e6b81e7fb954557dd7414b4dcea81fb30
LVBK <levanbaokhanh.bkhn@gmail.com>
2018-03-16 12:12:29 +0700

    basic static treeview

--- src/main/resources/static/custom/js/pages/enviromentSettings/form.js
+++ src/main/resources/static/custom/js/pages/enviromentSettings/form.js
@@ -7,12 +7,64 @@
     $(function () {
         setFormChangeListener();
         setGoBackListener('backBtn');
+        setOpenModalListener('openDirectoriesModal')
     });
 
     function setFormChangeListener() {
         $('#enviromentSettingsForm').change(function() {
             formChange = true;
         });
+    }
+
+    function getTree() {
+        // Some logic to retrieve, or generate tree structure
+        var tree = [
+            {
+                text: "Node 1",
+                state: {
+                    expanded: false,
+                    selected: true
+                },
+                tags: ['available'],
+                nodes: [
+                    {
+                        text: "Child 1",
+                        nodes: [
+                            {
+                                text: "Grandchild 1"
+                            },
+                            {
+                                text: "Grandchild 2"
+                            }
+                        ]
+                    },
+                    {
+                        text: "Child 2"
+                    }
+                ]
+            },
+            {
+                text: "Parent 2"
+            },
+            {
+                text: "Parent 3"
+            },
+            {
+                text: "Parent 4"
+            },
+            {
+                text: "Parent 5"
+            }
+        ];
+        return tree;
+    }
+    
+    function setOpenModalListener(name) {
+        $("button[name='"+name+"']").click(function () {
+            console.log("ahaha");
+            $('#tree').treeview({data: getTree()});
+            $('#directoriesModal').modal();
+        })
     }
 
     function setGoBackListener(name){
