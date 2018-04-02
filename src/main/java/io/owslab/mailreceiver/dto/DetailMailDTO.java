4144803eb1471ea1784cce533b791da1d91d7645
LVBK <levanbaokhanh.bkhn@gmail.com>
2018-04-02 14:27:15 +0700

    update result mail content

--- src/main/java/io/owslab/mailreceiver/dto/DetailMailDTO.java
+++ src/main/java/io/owslab/mailreceiver/dto/DetailMailDTO.java
@@ -1,5 +1,6 @@
 package io.owslab.mailreceiver.dto;
 
+import com.mariten.kanatools.KanaConverter;
 import io.owslab.mailreceiver.model.AttachmentFile;
 import io.owslab.mailreceiver.model.Email;
 import org.apache.commons.lang.time.DateFormatUtils;
@@ -157,7 +158,11 @@ public class DetailMailDTO {
     }
 
     public void setOriginalBody(String originalBody) {
-        this.originalBody = originalBody;
+        int conv_op_flags = 0;
+        conv_op_flags |= KanaConverter.OP_HAN_KATA_TO_ZEN_KATA;
+        conv_op_flags |= KanaConverter.OP_ZEN_ASCII_TO_HAN_ASCII;
+        String japaneseOptimizedText = KanaConverter.convertKana(originalBody, conv_op_flags);
+        this.originalBody = japaneseOptimizedText;
     }
 
     public List<FileDTO> getFiles() {
