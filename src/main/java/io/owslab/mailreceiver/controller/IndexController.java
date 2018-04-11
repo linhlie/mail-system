09b999f99ce6c6439d1ee089bdab9c7dac5e4d73
LVBK <levanbaokhanh.bkhn@gmail.com>
2018-04-11 10:45:46 +0700

    mail progress dashboard

--- src/main/java/io/owslab/mailreceiver/controller/IndexController.java
+++ src/main/java/io/owslab/mailreceiver/controller/IndexController.java
@@ -1,5 +1,6 @@
 package io.owslab.mailreceiver.controller;
 
+import io.owslab.mailreceiver.service.mail.FetchMailsService;
 import io.owslab.mailreceiver.service.mail.MailBoxService;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
@@ -9,12 +10,20 @@ import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
 
 import javax.servlet.http.HttpServletRequest;
+import java.text.DateFormat;
+import java.text.SimpleDateFormat;
+import java.util.Date;
 
 @Controller
 public class IndexController {
 
     @Autowired
     private MailBoxService mailBoxService;
+
+    @Autowired
+    private FetchMailsService fetchMailsService;
+
+    private DateFormat df = new SimpleDateFormat("yyyy/MM/dd HH:mm:ss");
 
     @RequestMapping("/default")
 
@@ -37,6 +46,12 @@ public class IndexController {
     public String admin(Model model) {
         long numberOfMessage = mailBoxService.count();
         model.addAttribute("numberOfMessage", numberOfMessage);
+        FetchMailsService.FetchMailProgress mailProgress = fetchMailsService.getTotalFetchMailProgress();
+        model.addAttribute("mailProgressDone", mailProgress.getDone());
+        model.addAttribute("mailProgressTotal", mailProgress.getTotal());
+        double percent = mailProgress.getTotal() == 0 ? 0 : ((double)mailProgress.getDone() / (double)mailProgress.getTotal()) * 100;
+        model.addAttribute("mailProgressPercent", (int) percent);
+        model.addAttribute("mailProgressLastUpdate", df.format(new Date()));
         return "admin";
     }
 
