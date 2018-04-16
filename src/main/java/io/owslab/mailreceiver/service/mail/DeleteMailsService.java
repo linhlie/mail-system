2977c16aec0ee3e0c99e5ba2d69bce7eaf7a6d58
LVBK <levanbaokhanh.bkhn@gmail.com>
2018-04-16 09:57:37 +0700

    delete mail, file data end file sotraged

--- src/main/java/io/owslab/mailreceiver/service/mail/DeleteMailsService.java
+++ src/main/java/io/owslab/mailreceiver/service/mail/DeleteMailsService.java
@@ -4,11 +4,14 @@ import io.owslab.mailreceiver.dao.EmailDAO;
 import io.owslab.mailreceiver.dao.FileDAO;
 import io.owslab.mailreceiver.model.AttachmentFile;
 import io.owslab.mailreceiver.model.Email;
+import org.apache.commons.io.FileUtils;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
+import java.io.File;
+import java.io.IOException;
 import java.util.Date;
 import java.util.List;
 
@@ -32,18 +35,7 @@ public class DeleteMailsService {
         for(int i = 0, n = emailList.size(); i < n; i++){
             Email email = emailList.get(i);
             logger.info("Start delete mail: " + email.getSubject());
-            //Update: delete mail and mark as deleted
-            email.setCc(null);
-            email.setBcc(null);
-            email.setReplyTo(null);
-            email.setReceivedAt(null);
-            email.setOptimizedBody(null);
-            email.setOriginalBody(null);
-            email.setHeader(null);
-            email.setMetaData(null);
-            email.setDeleted(true);
-            email.setDeletedAt(new Date());
-            emailDAO.save(email);
+            emailDAO.delete(email);
             //TODO: transac with delete mail and delete file?
             this.deleteFileBelongToMail(email.getMessageId());
         }
@@ -53,12 +45,14 @@ public class DeleteMailsService {
     public void deleteFileBelongToMail(String messageId){
         List<AttachmentFile> files = fileDAO.findByMessageIdAndDeleted(messageId, false);
         for(int i = 0, n = files.size(); i < n; i++){
-            AttachmentFile file = files.get(i);
-            //TODO: delete file storaged
-            file.setMetaData(null);
-            file.setDeleted(true);
-            file.setDeletedAt(new Date());
-            fileDAO.save(file);
+            AttachmentFile fileDoc = files.get(i);
+            File file = new File(fileDoc.getStoragePath());
+            try {
+                FileUtils.deleteDirectory(file);
+            } catch (IOException e) {
+                e.printStackTrace();
+            }
+            fileDAO.delete(fileDoc.getId());
         }
     }
 }
