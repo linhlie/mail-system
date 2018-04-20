5e8cbd46071b997e68268d2f07d5ffa6707b434e
LVBK <levanbaokhanh.bkhn@gmail.com>
2018-04-20 16:03:18 +0700

    add regex for matching comma

--- src/main/java/io/owslab/mailreceiver/utils/Utils.java
+++ src/main/java/io/owslab/mailreceiver/utils/Utils.java
@@ -2,10 +2,10 @@ package io.owslab.mailreceiver.utils;
 
 import org.springframework.cache.annotation.Cacheable;
 
-import java.text.ParseException;
-import java.text.SimpleDateFormat;
+import java.text.*;
 import java.util.Calendar;
 import java.util.Date;
+import java.util.Locale;
 import java.util.TimeZone;
 import java.util.concurrent.TimeUnit;
 
@@ -40,5 +40,9 @@ public class Utils {
     public synchronized static Date parseDateStr(final String str)
             throws ParseException {
         return DATE_FORMAT.parse(str);
+    }
+
+    public synchronized static String formatNumber(double number){
+        return java.text.NumberFormat.getNumberInstance(Locale.US).format(number);
     }
 }
