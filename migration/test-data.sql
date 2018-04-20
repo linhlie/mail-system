38ea519d3423663116a6ddf41d7ff9876d39fc50
LVBK <levanbaokhanh.bkhn@gmail.com>
2018-04-20 09:35:26 +0700

    change replace letter collate

--- migration/test-data.sql
+++ migration/test-data.sql
@@ -1,19 +1,14 @@
 SET NAMES utf8 COLLATE utf8_unicode_ci;
 SET FOREIGN_KEY_CHECKS=0;
 
--- INSERT INTO email_account_settings(account, password, mail_server_address, mail_server_port)
---     VALUES ('khanhlvb@ows.vn', 'Lekhanh281', 'imap.gmail.com', 993);
---
--- INSERT INTO email_account_settings(account, password, mail_server_address, mail_server_port)
---     VALUES ('ows-test@world-link-system.com', 'o2018wa01e', 'af125.secure.ne.jp', 993);
-
 INSERT INTO `email_accounts` (`id`, `account`, `disabled`)
 VALUES
-	(6, 'section-sales@ipc-japan.com', 0);
+	(7, 'section-sales@world-link-system.com', 0);
 
 INSERT INTO `email_account_settings` (`id`, `account_id`, `user_name`, `password`, `mail_server_address`, `mail_server_port`, `mail_protocol`, `encryption_protocol`, `authentication_protocol`, `proxy_server`, `created_at`, `updated_at`, `type`)
 VALUES
-	(4, 6, '', 'IpSecSa95953', 'af152.secure.ne.jp', 993, 0, 0, 0, '', NULL, '2018-03-27 14:41:51', 0);
+	(6, 7, 'section-sales@world-link-system.com', 'WlSecSa95953', 'af125.secure.ne.jp', 993, 0, 1, 0, '', NULL, '2018-04-18 08:43:36', 0),
+	(7, 7, 'section-sales@world-link-system.com', 'WlSecSa95953', 'af125.secure.ne.jp', 465, 2, 1, 0, '', NULL, '2018-04-18 08:43:36', 1);
 
 INSERT INTO replace_numbers(`character`, replace_value)
 VALUES ('K', 1000);
