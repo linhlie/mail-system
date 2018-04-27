9e8dfef3c620935db4b7f5b76502e19aac548dc1
LVBK <levanbaokhanh.bkhn@gmail.com>
2018-04-27 18:24:41 +0700

    save and restore conditions

--- src/main/resources/static/custom/js/pages/matching/settings.js
+++ src/main/resources/static/custom/js/pages/matching/settings.js
@@ -218,19 +218,6 @@
             setInputAutoComplete("matchingValue");
         });
 
-        $('#btn-get').on('click', function() {
-            var result = $(matchingBuilderId).queryBuilder('getRules');
-            if (!$.isEmptyObject(result)) {
-                alert(JSON.stringify(result, null, 2));
-            }
-        });
-
-        setAddReplaceLetterRowListener('addMotoRow', sourceTableId, ["group", "combine", "item", "condition", "value"]);
-        setAddReplaceLetterRowListener('addSakiRow', destinationTableId, ["group", "combine", "item", "condition", "value"]);
-        setAddReplaceLetterRowListener('addMatchRow', matchingTableId, ["group", "combine", "item", "condition", "value"]);
-        setRemoveRowListener("removeConditionRow");
-        getDefaultSourceListData();
-        getDefaultDestinationListData();
         setButtonClickListenter(saveSourceBtnId, saveSourceListData);
         setButtonClickListenter(getSourceBtnId, getSourceListData);
         setButtonClickListenter(saveDestinationBtnId, saveDestinationListData);
@@ -252,8 +239,6 @@
     function saveSourceListData(){
         var result = $(sourceBuilderId).queryBuilder('getRules');
         if ($.isEmptyObject(result)) return;
-        console.log("saveSourceListData: ", result);
-        return;
         var datalistKey = "/user/matchingSettings/listSourceKey";
         var datalistStr = localStorage.getItem(datalistKey);
         var datalist = JSON.parse(datalistStr);
@@ -264,11 +249,10 @@
                     datalist.push(name);
                 }
                 localStorage.setItem(datalistKey, JSON.stringify(datalist));
-                var data = buildDataFromTable(sourceTableId);
                 saveListData(
                     "/user/matchingSettings/source",
                     name,
-                    data
+                    result
                 )
             }
         })
@@ -303,8 +287,6 @@
     function saveDestinationListData(){
         var result = $(destinationBuilderId).queryBuilder('getRules');
         if ($.isEmptyObject(result)) return;
-        console.log("saveDestinationListData: ", result);
-        return;
         var datalistKey = "/user/matchingSettings/listDestinationKey";
         var datalistStr = localStorage.getItem(datalistKey);
         var datalist = JSON.parse(datalistStr);
@@ -315,11 +297,10 @@
                     datalist.push(name);
                 }
                 localStorage.setItem(datalistKey, JSON.stringify(datalist));
-                var data = buildDataFromTable(destinationTableId);
                 saveListData(
                     "/user/matchingSettings/destination",
                     name,
-                    data
+                    result
                 )
             }
         })
@@ -328,13 +309,6 @@
     function saveListData(url, name,  data) {
         var key = url + "@" + name;
         localStorage.setItem(key, JSON.stringify(data));
-    }
-
-    function setRemoveRowListener(name) {
-        $("span[name='"+name+"']").off('click');
-        $("span[name='"+name+"']").click(function () {
-            $(this)[0].parentNode.parentNode.className+=' hidden';
-        })
     }
     
     function setInputAutoComplete(className) {
@@ -350,16 +324,10 @@
             }
         });
     }
-
-    function setAddReplaceLetterRowListener(name, tableId, data) {
-        $("span[name='"+name+"']").click(function () {
-            addRow(tableId);
-        })
-    }
     
     function getSourceListData(skip) {
         if(skip){
-            getListData("/user/matchingSettings/source", null, sourceTableId, getSourceBtnId);
+            getListData("/user/matchingSettings/source", null, sourceBuilderId);
         } else {
             var datalistKey = "/user/matchingSettings/listSourceKey";
             var datalistStr = localStorage.getItem(datalistKey);
@@ -367,7 +335,7 @@
             datalist = datalist || [];
             showNamePrompt(datalist, function (name) {
                 if (name != null && name.length > 0) {
-                    getListData("/user/matchingSettings/source", name, sourceTableId, getSourceBtnId);
+                    getListData("/user/matchingSettings/source", name, sourceBuilderId);
                 }
             })
         }
@@ -375,7 +343,7 @@
 
     function getDestinationListData(skip) {
         if(skip){
-            getListData("/user/matchingSettings/destination", null, destinationTableId, getDestinationBtnId);
+            getListData("/user/matchingSettings/destination", null, destinationBuilderId);
         } else {
             var datalistKey = "/user/matchingSettings/listDestinationKey";
             var datalistStr = localStorage.getItem(datalistKey);
@@ -383,69 +351,21 @@
             datalist = datalist || [];
             showNamePrompt(datalist, function (name) {
                 if (name != null && name.length > 0) {
-                    getListData("/user/matchingSettings/destination", name, destinationTableId, getDestinationBtnId);
+                    getListData("/user/matchingSettings/destination", name, destinationBuilderId);
                 }
             })
         }
     }
 
-    // function getMatchingListData() {
-    //     var name = prompt("Please enter saved name", "");
-    //     if (name != null) {
-    //         getListData("/user/matchingSettings/matching", name, matchingTableId);
-    //     }
-    // }
-    
-    function getDefaultSourceListData() {
-        var data = [];
-        var condition = {
-            combine: "-1",
-            condition: "-1",
-            group: false,
-            id: "",
-            item: "7",
-            remove: 0,
-            value: "",
-        };
-        data.push(condition);
-        showDefaultListData(data, sourceTableId);
-    }
-
-    function getDefaultDestinationListData() {
-        var data = [];
-        var condition = {
-            combine: "-1",
-            condition: "-1",
-            group: false,
-            id: "",
-            item: "8",
-            remove: 0,
-            value: "",
-        };
-        data.push(condition);
-        showDefaultListData(data, destinationTableId);
-    }
-
-    function showDefaultListData(data, tableId) {
-        data = addDefaultReceiveDateRow(data);
-        for(var i = 0; i < data.length; i ++){
-            addRowWithData(tableId, data[i]);
-        }
-    }
-
-    function getListData(url, name, tableId) {
+    function getListData(url, name, builderId) {
         var data = null;
         if(name && name.length > 0){
             var key = url + "@" + name;
             data = localStorage.getItem(key) != null ? JSON.parse(localStorage.getItem(key)) : null;
         }
         if(data != null){
-            console.log("getListData: ", data);
-            removeAllRow(tableId);
             data = addDefaultReceiveDateRow(data);
-            for(var i = 0; i < data.length; i ++){
-                addRowWithData(tableId, data[i]);
-            }
+            $(builderId).queryBuilder('setRules', data);
         } else {
             alert("見つけませんでした。");
         }
@@ -453,88 +373,25 @@
 
     function addDefaultReceiveDateRow(data) {
         var receivedDateCondition = null;
-        for(var i = 0; i < data.length; i ++){
-            var condition = data[i];
-            if(condition.item == "9"){
-                receivedDateCondition = condition;
-            }
-        }
-        if(receivedDateCondition == null){
-            // var sevenDayAgo = new Date();
-            // sevenDayAgo.setDate(sevenDayAgo.getDate() - 7);
-            // var month = '' + (sevenDayAgo.getMonth() + 1);
-            // var day = '' + sevenDayAgo.getDate();
-            // var year = sevenDayAgo.getFullYear();
-            // if (month.length < 2) month = '0' + month;
-            // if (day.length < 2) day = '0' + day;
-
-            receivedDateCondition = {
-                combine: "0",
-                condition: "4",
-                group: true,
-                id: "",
-                item: "9",
-                remove: 0,
-                value: "-7",
-            }
-            data.push(receivedDateCondition);
-        }
-        return data;
-    }
-
-    function disableButton(buttonId, disabled) {
-        if(buttonId && buttonId.length > 0){
-            $(buttonId).prop("disabled", disabled);
-        }
-    }
-
-    function addRow(tableId) {
-        var table = document.getElementById(tableId);
-        var body = table.tBodies[0];
-        var rowToClone = table.rows[1];
-        var clone = rowToClone.cloneNode(true);
-        clone.className = undefined;
-        body.appendChild(clone);
-        setRemoveRowListener("removeConditionRow");
-        setInputAutoComplete("matchingValue");
-    }
-    
-    function addRowWithData(tableId, data) {
-        var table = document.getElementById(tableId);
-        var body = table.tBodies[0];
-        var rowToClone = table.rows[1];
-        var row = rowToClone.cloneNode(true);
-        row.className = undefined;
-        var cells = row.cells;
-        for(var i = 0; i < cells.length; i++){
-            var cell = cells.item(i);
-            var cellKey = cell.getAttribute("data");
-            if(!cellKey) continue;
-            var cellNode = cell.childNodes.length > 1 ? cell.childNodes[1] : cell.childNodes[0];
-            if(cellNode){
-                if(cellNode.nodeName == "INPUT") {
-                    if(cellNode.type == "checkbox"){
-                        cellNode.checked = data[cellKey];
-                    } else if(cellNode.type == "text"){
-                        cellNode.value = data[cellKey];
-                    }
-                } else if(cellNode.nodeName == "SELECT") {
-                    cellNode.value = data[cellKey];
-                } else if(cellNode.nodeName == "SPAN") {
-                    cellNode.textContent = data[cellKey];
+        if(data.rules) {
+            for(var i = 0; i < data.rules.length; i ++){
+                var condition = data.rules[i];
+                if(condition.id == "8"){
+                    receivedDateCondition = condition;
                 }
             }
         }
-        body.appendChild(row);
-        setRemoveRowListener("removeConditionRow");
-    }
-    
-    function removeAllRow(tableId) { //Except header row
-        var table = document.getElementById(tableId);
-        while(table.rows.length > 2){
-            var row = table.rows[2];
-            row.parentNode.removeChild(row);
+        if(receivedDateCondition == null){
+            receivedDateCondition = {
+                id: "8",
+                operator: "greater_or_equal",
+                type:  "string",
+                value: "-7"
+            }
+            data.rules = data.rules ? data.rules : [];
+            data.rules.push(receivedDateCondition);
         }
+        return data;
     }
     
     function buildDataFromTable(tableId) {
@@ -569,21 +426,6 @@
             data.push(rowData);
         }
         return data;
-    }
-    
-    function removeEmptyRowData(data) {
-        var result = [];
-        for (var i = 0; i < data.length; i++) {
-            var rowData = data[i];
-            if(!isEmptyRowData(rowData)) {
-                result.push(rowData);
-            }
-        }
-        return result;
-    }
-    
-    function isEmptyRowData(rowData) {
-        return (rowData["item"] === '-1' || rowData["item"] === -1);
     }
 
     function isSkipRow(row) {
