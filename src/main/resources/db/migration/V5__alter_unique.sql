3e87d67db3d8edcdef77b99f88b241dd01080e08
LVBK <levanbaokhanh.bkhn@gmail.com>
2018-05-14 08:57:52 +0700

    revert

--- src/main/resources/db/migration/V5__alter_unique.sql
+++ src/main/resources/db/migration/V5__alter_unique.sql
@@ -1,8 +1,8 @@
 ALTER TABLE fuzzy_words
-ADD CONSTRAINT uc_fuzzy_word UNIQUE KEY (word_id,with_word_id);
+ADD CONSTRAINT uc_fuzzy_word UNIQUE (word_id,with_word_id);
 
 ALTER TABLE email_word_jobs
-ADD CONSTRAINT uc_email_word_job UNIQUE KEY (message_id,word_id);
+ADD CONSTRAINT uc_email_word_job UNIQUE (message_id,word_id);
 
 ALTER TABLE emails_words
-ADD CONSTRAINT uc_email_word UNIQUE KEY (message_id,word_id);
+ADD CONSTRAINT uc_email_word UNIQUE (message_id,word_id);
