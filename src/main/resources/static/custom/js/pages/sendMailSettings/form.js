e1197ff5b3705d525bd5b1b4db615b786341ba7c
LVBK <levanbaokhanh.bkhn@gmail.com>
2018-06-08 17:21:13 +0700

    include attachment local storage

--- src/main/resources/static/custom/js/pages/sendMailSettings/form.js
+++ src/main/resources/static/custom/js/pages/sendMailSettings/form.js
@@ -2,7 +2,13 @@
 (function () {
 
     $(function () {
-
+        var cachedIncludeAttachmentStr = localStorage.getItem("includeAttachment");
+        var cachedIncludeAttachment = typeof cachedIncludeAttachmentStr !== "string" ? false : !!JSON.parse(cachedIncludeAttachmentStr);
+        $('#includeAttachment').prop('checked', cachedIncludeAttachment);
+        $('#includeAttachment').change(function() {
+            var includeAttachment = $(this).is(":checked");
+            localStorage.setItem("includeAttachment", includeAttachment);
+        });
     });
 
 })(jQuery);
