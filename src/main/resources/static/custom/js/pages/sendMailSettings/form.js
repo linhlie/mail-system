f38f449da70bdb5b18e40c58d09f6211acd23544
LVBK <levanbaokhanh.bkhn@gmail.com>
2018-07-26 09:59:17 +0700

    separate tab setting

--- src/main/resources/static/custom/js/pages/sendMailSettings/form.js
+++ src/main/resources/static/custom/js/pages/sendMailSettings/form.js
@@ -5,6 +5,9 @@
         var cachedIncludeAttachmentStr = localStorage.getItem("includeAttachment");
         var cachedIncludeAttachment = typeof cachedIncludeAttachmentStr !== "string" ? false : !!JSON.parse(cachedIncludeAttachmentStr);
         $('#includeAttachment').prop('checked', cachedIncludeAttachment);
+        var cachedSeparateTabStr = localStorage.getItem("separateTab");
+        var cachedSeparateTab = typeof cachedSeparateTabStr !== "string" ? false : !!JSON.parse(cachedSeparateTabStr);
+        $('#separateTab').prop('checked', cachedSeparateTab);
         var selectedSendMailAccountId = localStorage.getItem("selectedSendMailAccountId");
         if(selectedSendMailAccountId) {
             $("#sendMailAccountSelect option").each(function()
@@ -16,6 +19,10 @@
         $('#includeAttachment').change(function() {
             var includeAttachment = $(this).is(":checked");
             localStorage.setItem("includeAttachment", includeAttachment);
+        });
+        $('#separateTab').change(function() {
+            var separateTab = $(this).is(":checked");
+            localStorage.setItem("separateTab", separateTab);
         });
         $('#sendMailAccountSelect').on('change', function() {
             localStorage.setItem("selectedSendMailAccountId", this.value);
