910ecaf152f6c8a356817a4d483fa94d77239b5f
LVBK <levanbaokhanh.bkhn@gmail.com>
2018-08-01 11:36:02 +0700

    global config

--- src/main/resources/static/custom/js/pages/matching/settings.js
+++ src/main/resources/static/custom/js/pages/matching/settings.js
@@ -243,78 +243,12 @@
             operators: ['equal', 'not_equal']
         }];
 
-        var default_lang = {
-            "__locale": "English (en)",
-            "__author": "Damien \"Mistic\" Sorel, http://www.strangeplanet.fr",
-
-            "add_rule": "ルールの追加",
-            "add_group": "グループの追加",
-            "delete_rule": "削除",
-            "delete_group": "削除",
-
-            "conditions": {
-                "AND": "AND",
-                "OR": "OR"
-            },
-
-            "operators": {
-                "equal": "等しい ＝",
-                "not_equal": "異なる ≠",
-                "in": "いずれか ...",
-                "not_in": "いずれでもない",
-                "less": "未満 ＜",
-                "less_or_equal": "以下 ≦",
-                "greater": "超 ＞",
-                "greater_or_equal": "以上 ≧",
-                "between": "範囲",
-                "not_between": "範囲外",
-                "begins_with": "開始する",
-                "not_begins_with": "開始ではない",
-                "contains": "含む 〇",
-                "not_contains": "含まない ×",
-                "ends_with": "終了する",
-                "not_ends_with": "終了ではない",
-                "is_empty": "空",
-                "is_not_empty": "空でない",
-                "is_null": "空",
-                "is_not_null": "空でない"
-            },
-
-            "errors": {
-                "no_filter": "No filter selected",
-                "empty_group": "The group is empty",
-                "radio_empty": "No value selected",
-                "checkbox_empty": "No value selected",
-                "select_empty": "No value selected",
-                "string_empty": "Empty value",
-                "string_exceed_min_length": "Must contain at least {0} characters",
-                "string_exceed_max_length": "Must not contain more than {0} characters",
-                "string_invalid_format": "Invalid format ({0})",
-                "number_nan": "Not a number",
-                "number_not_integer": "Not an integer",
-                "number_not_double": "Not a real number",
-                "number_exceed_min": "Must be greater than {0}",
-                "number_exceed_max": "Must be lower than {0}",
-                "number_wrong_step": "Must be a multiple of {0}",
-                "number_between_invalid": "Invalid values, {0} is greater than {1}",
-                "datetime_empty": "Empty value",
-                "datetime_invalid": "Invalid date format ({0})",
-                "datetime_exceed_min": "Must be after {0}",
-                "datetime_exceed_max": "Must be before {0}",
-                "datetime_between_invalid": "Invalid values, {0} is greater than {1}",
-                "boolean_not_valid": "Not a boolean",
-                "operator_not_multiple": "Operator \"{1}\" cannot accept multiple values"
-            },
-            "invert": "Invert",
-            "NOT": "NOT"
-        }
-
         var default_source_configs = {
             plugins: default_plugins,
             allow_empty: true,
             filters: default_filters,
             rules: null,
-            lang: default_lang,
+            lang: globalConfig.default_lang,
         };
 
         var default_destination_configs = {
@@ -322,7 +256,7 @@
             allow_empty: true,
             filters: default_filters,
             rules: null,
-            lang: default_lang,
+            lang: globalConfig.default_lang,
         };
 
         var default_matching_configs = {
@@ -409,7 +343,7 @@
                 },
             }],
             rules: null,
-            lang: default_lang,
+            lang: globalConfig.default_lang,
         };
 
         $(sourceBuilderId).queryBuilder(default_source_configs);
