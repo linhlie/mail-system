df13a28d5c72d6b8548505457abd3eee20e8213b
LVBK <levanbaokhanh.bkhn@gmail.com>
2018-08-24 17:00:28 +0700

    basic ui export import partner

--- src/main/resources/static/custom/js/pages/expansion/partnerImportExport.js
+++ src/main/resources/static/custom/js/pages/expansion/partnerImportExport.js
@@ -2,8 +2,31 @@
 (function () {
 
     $(function () {
-
+        setButtonClickListenter("#importGroupBtn", onImportGroupClick);
+        setButtonClickListenter("#importPartnerBtn", onImportPartnerClick);
+        setImportFileChangeListener()
     });
+    
+    function onImportPartnerClick() {
+        $("#importPartnerInput").click();
+    }
+    
+    function onImportGroupClick() {
+        $("#importGroupInput").click();
+    }
+    
+    function setImportFileChangeListener() {
+        $("#importPartnerInput").change(function (){
+            var fileName = $(this).val();
+            fileName = fileName.replace(/C:\\fakepath\\/i, '');
+            $("#importPartner").val(fileName);
+        });
+        $("#importGroupInput").change(function (){
+            var fileName = $(this).val();
+            fileName = fileName.replace(/C:\\fakepath\\/i, '');
+            $("#importGroup").val(fileName);
+        });
+    }
 
 
 })(jQuery);
