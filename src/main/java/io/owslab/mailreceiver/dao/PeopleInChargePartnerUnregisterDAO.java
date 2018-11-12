8a9a76a4a095d707cbeb1db858ccc76630dd16c4
Huy NB <huynb@ows.com.vn>
2018-11-12 16:31:39 +0700

    create model/db

---
+++ src/main/java/io/owslab/mailreceiver/dao/PeopleInChargePartnerUnregisterDAO.java
@@ -0,0 +1,7 @@
+package io.owslab.mailreceiver.dao;
+
+import io.owslab.mailreceiver.model.PeopleInChargePartnerUnregister;
+import org.springframework.data.jpa.repository.JpaRepository;
+
+public interface PeopleInChargePartnerUnregisterDAO extends JpaRepository<PeopleInChargePartnerUnregister, Long> {
+}
