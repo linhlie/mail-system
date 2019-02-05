7e556e9badbe4d4184f1d5f4a6c6dd5464eb2f81
Huy NB <huynb@ows.com.vn>
2019-02-05 14:33:32 +0700

    create UI

--- src/main/java/io/owslab/mailreceiver/dao/ConditionNotificationDAO.java
+++ src/main/java/io/owslab/mailreceiver/dao/ConditionNotificationDAO.java
@@ -2,7 +2,18 @@ package io.owslab.mailreceiver.dao;
 
 import io.owslab.mailreceiver.model.ConditionNotification;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Modifying;
+import org.springframework.data.jpa.repository.Query;
+import org.springframework.data.repository.query.Param;
+
+import java.util.List;
 
 public interface ConditionNotificationDAO extends JpaRepository<ConditionNotification, Long> {
 
+    @Modifying(clearAutomatically = true)
+    @Query(
+            value = "SELECT * FROM condition_notification c where c.from_account_id = :fromAccountId and c.condition_type = :conditionType order by send_at desc limit :listSize;",
+            nativeQuery = true
+    )
+    List<ConditionNotification> findByToAccountIdAndConditionTypeLimit(@Param("fromAccountId") long fromAccountId, @Param("conditionType") int conditionType, @Param("listSize") int listSize);
 }
--- src/main/java/io/owslab/mailreceiver/service/condition/conditionNotificationService.java
+++ src/main/java/io/owslab/mailreceiver/service/condition/conditionNotificationService.java
@@ -4,29 +4,49 @@ import io.owslab.mailreceiver.dao.ConditionNotificationDAO;
 import io.owslab.mailreceiver.model.ConditionNotification;
 import io.owslab.mailreceiver.service.security.AccountService;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.PageRequest;
+import org.springframework.data.domain.Sort;
 import org.springframework.stereotype.Service;
 
+import java.util.ArrayList;
 import java.util.Date;
 import java.util.List;
 
 @Service
 public class conditionNotificationService {
-
+    private static final int PAGE_SIZE = 10;
     @Autowired
     AccountService accountService;
 
     @Autowired
     ConditionNotificationDAO conditionDAO;
 
-    public void save(ConditionNotification conditionNotification) throws Exception {
+    public void add(ConditionNotification conditionNotification) throws Exception {
         if(conditionNotification == null || conditionNotification.getCondition() == null){
-            throw new Exception("Can't save condition notification");
+            throw new Exception("Can't add condition notification");
         }
         conditionNotification.setFromAccountId(accountService.getLoggedInAccountId());
         conditionNotification.setSentAt(new Date());
         conditionDAO.save(conditionNotification);
     }
 
-//    public List<ConditionNotification> getConditionNotifications(long acccountId, int conditionType){
-//    }
+    public void update(ConditionNotification conditionNotification) throws Exception {
+        if(conditionNotification == null){
+            throw new Exception("Can't update condition notification");
+        }
+        ConditionNotification condition = conditionDAO.findOne(conditionNotification.getId());
+        if(condition == null){
+            return;
+        }else{
+            condition.setStatus(conditionNotification.getStatus());
+            conditionDAO.save(condition);
+        }
+    }
+
+    public List<ConditionNotification> getConditionNotifications(long acccountId, int conditionType, int pageNumber){
+        int listSize = pageNumber * PAGE_SIZE;
+        List<ConditionNotification> list = conditionDAO.findByToAccountIdAndConditionTypeLimit(acccountId, conditionType, listSize);
+        return list;
+    }
 }
